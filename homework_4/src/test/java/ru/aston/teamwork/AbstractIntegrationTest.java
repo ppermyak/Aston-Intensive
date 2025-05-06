86c94732b7ab013a70d8fdd125e05946682c187f
Sabur Sabirzyanov <sabursabirzyanov@yandex.ru>
2025-05-06 15:34:49 +0300

    - Добавлена аннотация @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) в тестовые классы UserService для обеспечения изоляции тестов. - Теперь контекст Spring будет очищаться после каждого теста, что предотвращает проблемы с зависимостями между тестами и позволяет корректно выполнять тесты в рамках одного запуска. - Это решение помогает устранить ошибки, связанные с состоянием контекста после выполнения первого теста. - Все тесты проходят успешно, покрытие улучшено

--- homework_4/src/test/java/ru/aston/teamwork/AbstractIntegrationTest.java
+++ homework_4/src/test/java/ru/aston/teamwork/AbstractIntegrationTest.java
@@ -3,9 +3,11 @@ package ru.aston.teamwork;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -15,26 +17,29 @@ import ru.aston.teamwork.repository.UserRepository;
 @SpringBootTest
 @Testcontainers
 @ActiveProfiles("test")
+@Transactional
+@Rollback
 public abstract class AbstractIntegrationTest {
 
     @Autowired
-    private UserRepository userRepository;
+    protected UserRepository userRepository;
 
 
     @Container
-    private static final PostgreSQLContainer<?> postgres =
+    public static final PostgreSQLContainer<?> postgres =
             new PostgreSQLContainer<>("postgres:16-alpine")
-            .withDatabaseName("testdb")
-            .withUsername("testuser")
-            .withPassword("testpass");
-
+                    .withDatabaseName("testdb")
+                    .withUsername("testuser")
+                    .withPassword("testpass");
 
 
     @DynamicPropertySource
-    static void postgresProperties(DynamicPropertyRegistry registry){
+    static void postgresProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+
+        System.out.println("PostgreSQL URL: " + postgres.getJdbcUrl());
     }
 
     @BeforeEach
--- homework_4/src/test/java/ru/aston/teamwork/service/AbstractUserServiceTest.java
+++ homework_4/src/test/java/ru/aston/teamwork/service/AbstractUserServiceTest.java
@@ -2,17 +2,15 @@ package ru.aston.teamwork.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.aston.teamwork.AbstractIntegrationTest;
 import ru.aston.teamwork.dto.UserRequestDto;
-import ru.aston.teamwork.repository.UserRepository;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class AbstractUserServiceTest extends AbstractIntegrationTest {
 
     @Autowired
     protected UserService userService;
-
-    @Autowired
-    protected UserRepository userRepository;
 
     protected UserRequestDto testUserDto;
 
